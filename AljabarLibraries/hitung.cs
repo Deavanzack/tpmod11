0f6606920b549d362ae296b2cfc9129858edb623
Deavanzack <daffafadillah564@gmail.com>
2024-05-06 04:57:39 +0700

    semua project

---
+++ AljabarLibraries/hitung.cs
@@ -0,0 +1,28 @@
+﻿namespace AljabarLibraries
+{
+    public class hitung
+    {
+        public static double[] AkarPersamaanKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double c = persamaan[2];
+
+            double diskriminan = b * b - 4 * a * c;
+            double x1 = (-b + Math.Sqrt(diskriminan))/2*a;
+            double x2 = (-b - Math.Sqrt(diskriminan)) / 2*a;
+            return new double[] { x1, x2 };
+        }
+
+        public static double[] HasilKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+
+            double c = a * a;
+            double d = 2 * a * b;
+            double e = b * b;
+            return new double[] { c, d, e };
+        }
+    }
+}
---
+++ aljabar/Program.cs
@@ -0,0 +1,17 @@
+﻿using AljabarLibraries;
+using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double[] persamaan = { 1, -3, -10 };
+        Console.WriteLine($"akar persamaan kuadrat dari {persamaan[0]}x^2 {persamaan[1]}x {persamaan[2]} adalah");
+        double[] akar = hitung.AkarPersamaanKuadrat(persamaan);
+        Console.WriteLine($"hasilnya: {akar[0]} dan {akar[1]}");
+
+        double[] persamaanLinear = { 2, -3 };
+        double[] hasil = hitung.HasilKuadrat(persamaanLinear);
+        Console.WriteLine($"\nhasil kuadrat dari persamaan {persamaanLinear[0]}x {persamaanLinear[1]} adalah");
+        Console.WriteLine($"Hasil kuadrat: {hasil[0]}x^2 + {hasil[1]}x + {hasil[2]}");
+    }
+}
